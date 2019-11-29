85e7a27ccdbef21dd5714612578082bf74c865c2
Tom <thomas.mcsweeney@student.manchester.ac.uk>
2019-11-29 13:45:24 +0000

    finished draft of paper

--- simulator/scripts/HOFT/hoft.py
+++ simulator/scripts/HOFT/hoft.py
@@ -45,52 +45,54 @@ multiple = Environment.Node(28, 4, name="Multiple_GPU")
 
 """Cholesky."""
 
-#start = timer()
-#heuristics = ["HEFT-WM", "HOFT"]
-#n_tasks = [35, 220, 680, 1540, 2925, 4960, 7770, 11480, 16215, 22100]
-#chol_speedup, chol_mkspans = defaultdict(lambda: defaultdict(lambda: defaultdict(list))), defaultdict(lambda: defaultdict(lambda: defaultdict(list))),
-#for nb in [128, 1024]:
-#    for env in [single, multiple]:
-#        with open("results/cholesky/{}_nb{}.txt".format(env.name, nb), "w") as dest:            
-#            env.print_info(filepath=dest)
-#            for nt in n_tasks:
-#                dag = nx.read_gpickle('../../graphs/cholesky/nb{}/{}tasks.gpickle'.format(nb, nt))
-#                dag.print_info(filepath=dest)
-#                mst = dag.minimal_serial_time(platform=env)
-#                print("Minimal serial time: {}\n".format(mst), file=dest)   
-#                chol_mkspans[env.name][nb]["MST"].append(mst)
-#                
-#                OFT = dag.optimistic_finish_times()    
-#
-#                heft_mkspan = HEFT(dag, platform=env)                
-#                chol_mkspans[env.name][nb]["HEFT"].append(heft_mkspan)
-#                print("HEFT makespan: {}\n".format(heft_mkspan), file=dest)  
-#                
-#                for h in heuristics:
-#                    if h == "HEFT-WM":
-#                        task_list = dag.sort_by_upward_rank(platform=env, weighting="WM-I")
-#                        mkspan = HEFT(dag, platform=env, task_list=task_list)
-#                    elif h == "HOFT":
-#                        task_list = OFT_priorities(dag, platform=env, selection="HOFT", table=OFT)
-#                        mkspan = HOFT(dag, platform=env, policy="HOFT", task_list=task_list)
-#                    chol_mkspans[env.name][nb][h].append(mkspan)  
-#                    sp = 100 - (mkspan / heft_mkspan) * 100
-#                    chol_speedup[env.name][nb][h].append(sp)
-#                    print("{} makespan: {}".format(h, mkspan), file=dest)           
-#                    print("Speedup (%) over HEFT: {}\n".format(sp), file=dest)                          
-#                
-#                print("--------------------------------------------------------\n", file=dest)                  
-#    
-## Save the makespans so can plot later if I want.
-#with open('data/chol_mkspans.dill'.format(nb), 'wb') as handle:
-#    dill.dump(chol_mkspans, handle)
-#    
-## Save the speedups so can plot later if I want.
-#with open('data/chol_speedups.dill'.format(nb), 'wb') as handle:
-#    dill.dump(chol_speedup, handle)
-#    
-#elapsed = timer() - start
-#print("Cholesky part took {} minutes".format(elapsed / 60))
+start = timer()
+heuristics = ["HEFT-WM", "HOFT", "HOFT-WM"]
+n_tasks = [35, 220, 680, 1540, 2925, 4960, 7770, 11480, 16215, 22100]
+chol_speedup, chol_mkspans = defaultdict(lambda: defaultdict(lambda: defaultdict(list))), defaultdict(lambda: defaultdict(lambda: defaultdict(list))),
+for nb in [128, 1024]:
+    for env in [single, multiple]:
+        with open("results/cholesky/{}_nb{}.txt".format(env.name, nb), "w") as dest:            
+            env.print_info(filepath=dest)
+            for nt in n_tasks:
+                dag = nx.read_gpickle('../../graphs/cholesky/nb{}/{}tasks.gpickle'.format(nb, nt))
+                dag.print_info(filepath=dest)
+                mst = dag.minimal_serial_time(platform=env)
+                print("Minimal serial time: {}\n".format(mst), file=dest)   
+                chol_mkspans[env.name][nb]["MST"].append(mst)
+                
+                OFT = dag.optimistic_finish_times()    
+
+                heft_mkspan = HEFT(dag, platform=env)                
+                chol_mkspans[env.name][nb]["HEFT"].append(heft_mkspan)
+                print("HEFT makespan: {}\n".format(heft_mkspan), file=dest)  
+                
+                for h in heuristics:
+                    if h == "HEFT-WM":
+                        task_list = dag.sort_by_upward_rank(platform=env, weighting="WM-I")
+                        mkspan = HEFT(dag, platform=env, task_list=task_list)
+                    elif h == "HOFT":
+                        task_list = OFT_priorities(dag, platform=env, selection="HOFT", table=OFT)
+                        mkspan = HOFT(dag, platform=env, policy="HOFT", task_list=task_list)
+                    elif h == "HOFT-WM":
+                        task_list = dag.sort_by_upward_rank(platform=env, weighting="WM-I")
+                        mkspan = HOFT(dag, platform=env, policy="HOFT", table=OFT, task_list=task_list) 
+                    chol_mkspans[env.name][nb][h].append(mkspan)  
+                    sp = 100 - (mkspan / heft_mkspan) * 100
+                    chol_speedup[env.name][nb][h].append(sp)
+                    print("{} makespan: {}".format(h, mkspan), file=dest)           
+                    print("Speedup (%) over HEFT: {}\n".format(sp), file=dest)     
+                print("--------------------------------------------------------\n", file=dest)                  
+    
+# Save the makespans so can plot later if I want.
+with open('data/chol_mkspans.dill'.format(nb), 'wb') as handle:
+    dill.dump(chol_mkspans, handle)
+    
+# Save the speedups so can plot later if I want.
+with open('data/chol_speedups.dill'.format(nb), 'wb') as handle:
+    dill.dump(chol_speedup, handle)
+    
+elapsed = timer() - start
+print("Cholesky part took {} minutes".format(elapsed / 60))
 
 """Plot the Cholesky speedups over HEFT."""
 
@@ -160,126 +162,126 @@ multiple = Environment.Node(28, 4, name="Multiple_GPU")
 
 
 
-"""Random."""
-start = timer()
-heuristics = ["HEFT-WM", "HOFT", "HOFT-WM"]
-n_dags = 180
-rand_mkspans = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(list))))
-rand_speedups = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(list))))
-rand_ccrs = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
-for env in [single, multiple]:
-    env.print_info()
-    for acc in ["low_acc", "high_acc"]:
-        for ccr in ["0_10", "10_20", "20_50"]:
-            makespans = defaultdict(list)
-            best_occurences, failures = defaultdict(int), defaultdict(int)
-            with open("results/random/{}/{}_CCR_{}.txt".format(env.name, acc, ccr), "w") as dest:            
-                env.print_info(filepath=dest)
-                count = 0
-                for app in os.listdir('../../graphs/random/{}/{}/CCR_{}'.format(env.name, acc, ccr)):
-                    count += 1
-#                    if count > 2: # REMEMBER TO REMOVE THIS BEFORE LEAVING TO RUN!
-#                        break
-                    print("Starting DAG number {}...".format(count))
-                    dag = nx.read_gpickle('../../graphs/random/{}/{}/CCR_{}/{}'.format(env.name, acc, ccr, app))
-                    dag.print_info(platform=env, filepath=dest) 
-                    rand_ccrs[env.name][acc][ccr].append(dag.CCR[env.name])
-                    mst = dag.minimal_serial_time(env) 
-                    rand_mkspans[env.name][acc][ccr]["MST"].append(mst)
-                    
-                    heft_mkspan = HEFT(dag, platform=env)                
-                    rand_mkspans[env.name][acc][ccr]["HEFT"].append(heft_mkspan)
-                    print("HEFT makespan: {}\n".format(heft_mkspan), file=dest)  
-                                    
-                    # Need to compute table for the OFT weightings.
-                    OFT = dag.optimistic_finish_times() 
-                    
-                    best = float('inf')                
-                    for h in heuristics:
-                        if h == "HEFT-WM":
-                            task_list = dag.sort_by_upward_rank(platform=env, weighting="WM-I")
-                            mkspan = HEFT(dag, platform=env, task_list=task_list)
-                        elif h == "HOFT":
-                            task_list = OFT_priorities(dag, platform=env, selection="HOFT", table=OFT)
-                            mkspan = HOFT(dag, platform=env, policy="HOFT", table=OFT, task_list=task_list)  
-                        elif h == "HOFT-WM":
-                            task_list = dag.sort_by_upward_rank(platform=env, weighting="WM-I")
-                            mkspan = HOFT(dag, platform=env, policy="HOFT", table=OFT, task_list=task_list)  
-                        rand_mkspans[env.name][acc][ccr][h].append(mkspan)
-                        sp = 100 - (mkspan / heft_mkspan) * 100
-                        rand_speedups[env.name][acc][ccr][h].append(sp)
-                        print("{} makespan: {}".format(h, mkspan), file=dest)           
-                        print("Speedup (%) over HEFT: {}\n".format(sp), file=dest) 
-                        best = min(best, mkspan)                  
-                    print("--------------------------------------------------------\n", file=dest)                
-                    for h in ["HEFT"] + heuristics:
-                        m = rand_mkspans[env.name][acc][ccr][h][-1]
-                        if m == best:
-                            best_occurences[h] += 1
-                        if m > mst:
-                            failures[h] += 1                     
-                            
-                print("--------------------------------------------------------", file=dest)
-                print("SUMMARY", file=dest)
-                print("--------------------------------------------------------", file=dest)            
-                for h in ["HEFT"] + heuristics:
-                    print("{}.".format(h), file=dest)
-                    if h != "HEFT":
-                        print("Average improvement compared to HEFT: {}%".format(np.mean(rand_speedups[env.name][acc][ccr][h])), file=dest)
-                        print("Number of times better than HEFT: {}/{}".format(sum(1 for s in rand_speedups[env.name][acc][ccr][h] if s >= 1.0), n_dags), file=dest)               
-                    print("Number of best occurences: {}/{}".format(best_occurences[h], n_dags), file=dest)
-                    print("Number of failures: {}/{}\n".format(failures[h], n_dags), file=dest)          
-                
-elapsed = timer() - start
-print("This took {} minutes".format(elapsed / 60))
-
-# Save the makespans so can plot again later...
-with open('data/rand_mkspans.dill', 'wb') as handle:
-    dill.dump(rand_mkspans, handle)
-    
-# Save the speedups so can plot again later...
-with open('data/rand_speedups.dill', 'wb') as handle:
-    dill.dump(rand_speedups, handle)
-    
-# Save the CCRs so can plot again later...
-with open('data/rand_ccrs.dill', 'wb') as handle:
-    dill.dump(rand_ccrs, handle)
-    
-    
-"""Plot the speedup over HEFT for the random DAGs."""
-
-with open('data/rand_speedups.dill', 'rb') as file:
-    rand_speedups = dill.load(file)
-    
-with open('data/rand_ccrs.dill', 'rb') as file:
-    rand_ccrs = dill.load(file)
-    
-with open('data/rand_mkspans.dill', 'rb') as file:
-    rand_mkspans = dill.load(file)
-
-all_ccrs = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
-all_speedups = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
-
-for env in [single, multiple]:
-    for acc in ["low_acc", "high_acc"]:
-        for ccr in ["0_10", "10_20", "20_50"]:
-            for i in range(len(rand_mkspans[env.name][acc][ccr]["MST"])):
-                m = rand_mkspans[env.name][acc][ccr]["HEFT"][i]
-                for h in heuristics:
-                    m1 = rand_mkspans[env.name][acc][ccr][h][i]
-                    d1, d2 = rand_ccrs[env.name][acc][ccr][i], rand_speedups[env.name][acc][ccr][h][i]
-                    all_ccrs[env.name][acc][h].append(d1)
-                    all_speedups[env.name][acc][h].append(d2)
-                    
-                    
-with open("results/random/summary.txt", "w") as dest: 
-    for env in [single, multiple]:  
-        print("Platform: {}".format(env.name.replace('_', ' ')), file=dest)                
-        for acc in ["low_acc", "high_acc"]:
-            print("\n{} ACCELERATION".format("LOW" if acc == "low_acc" else "HIGH"), file=dest)
-            for h in heuristics:
-                print("{} mean speedup: {}".format(h, np.mean(all_speedups[env.name][acc][h])), file=dest)
-        print("\n\n\n\n\n", file=dest)      
+#"""Random."""
+#start = timer()
+#heuristics = ["HEFT-WM", "HOFT", "HOFT-WM"]
+#n_dags = 180
+#rand_mkspans = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(list))))
+#rand_speedups = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(list))))
+#rand_ccrs = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+#for env in [single, multiple]:
+#    env.print_info()
+#    for acc in ["low_acc", "high_acc"]:
+#        for ccr in ["0_10", "10_20", "20_50"]:
+#            makespans = defaultdict(list)
+#            best_occurences, failures = defaultdict(int), defaultdict(int)
+#            with open("results/random/{}/{}_CCR_{}.txt".format(env.name, acc, ccr), "w") as dest:            
+#                env.print_info(filepath=dest)
+#                count = 0
+#                for app in os.listdir('../../graphs/random/{}/{}/CCR_{}'.format(env.name, acc, ccr)):
+#                    count += 1
+##                    if count > 2: # REMEMBER TO REMOVE THIS BEFORE LEAVING TO RUN!
+##                        break
+#                    print("Starting DAG number {}...".format(count))
+#                    dag = nx.read_gpickle('../../graphs/random/{}/{}/CCR_{}/{}'.format(env.name, acc, ccr, app))
+#                    dag.print_info(platform=env, filepath=dest) 
+#                    rand_ccrs[env.name][acc][ccr].append(dag.CCR[env.name])
+#                    mst = dag.minimal_serial_time(env) 
+#                    rand_mkspans[env.name][acc][ccr]["MST"].append(mst)
+#                    
+#                    heft_mkspan = HEFT(dag, platform=env)                
+#                    rand_mkspans[env.name][acc][ccr]["HEFT"].append(heft_mkspan)
+#                    print("HEFT makespan: {}\n".format(heft_mkspan), file=dest)  
+#                                    
+#                    # Need to compute table for the OFT weightings.
+#                    OFT = dag.optimistic_finish_times() 
+#                    
+#                    best = float('inf')                
+#                    for h in heuristics:
+#                        if h == "HEFT-WM":
+#                            task_list = dag.sort_by_upward_rank(platform=env, weighting="WM-I")
+#                            mkspan = HEFT(dag, platform=env, task_list=task_list)
+#                        elif h == "HOFT":
+#                            task_list = OFT_priorities(dag, platform=env, selection="HOFT", table=OFT)
+#                            mkspan = HOFT(dag, platform=env, policy="HOFT", table=OFT, task_list=task_list)  
+#                        elif h == "HOFT-WM":
+#                            task_list = dag.sort_by_upward_rank(platform=env, weighting="WM-I")
+#                            mkspan = HOFT(dag, platform=env, policy="HOFT", table=OFT, task_list=task_list)  
+#                        rand_mkspans[env.name][acc][ccr][h].append(mkspan)
+#                        sp = 100 - (mkspan / heft_mkspan) * 100
+#                        rand_speedups[env.name][acc][ccr][h].append(sp)
+#                        print("{} makespan: {}".format(h, mkspan), file=dest)           
+#                        print("Speedup (%) over HEFT: {}\n".format(sp), file=dest) 
+#                        best = min(best, mkspan)                  
+#                    print("--------------------------------------------------------\n", file=dest)                
+#                    for h in ["HEFT"] + heuristics:
+#                        m = rand_mkspans[env.name][acc][ccr][h][-1]
+#                        if m == best:
+#                            best_occurences[h] += 1
+#                        if m > mst:
+#                            failures[h] += 1                     
+#                            
+#                print("--------------------------------------------------------", file=dest)
+#                print("SUMMARY", file=dest)
+#                print("--------------------------------------------------------", file=dest)            
+#                for h in ["HEFT"] + heuristics:
+#                    print("{}.".format(h), file=dest)
+#                    if h != "HEFT":
+#                        print("Average improvement compared to HEFT: {}%".format(np.mean(rand_speedups[env.name][acc][ccr][h])), file=dest)
+#                        print("Number of times better than HEFT: {}/{}".format(sum(1 for s in rand_speedups[env.name][acc][ccr][h] if s >= 1.0), n_dags), file=dest)               
+#                    print("Number of best occurences: {}/{}".format(best_occurences[h], n_dags), file=dest)
+#                    print("Number of failures: {}/{}\n".format(failures[h], n_dags), file=dest)          
+#                
+#elapsed = timer() - start
+#print("This took {} minutes".format(elapsed / 60))
+#
+## Save the makespans so can plot again later...
+#with open('data/rand_mkspans.dill', 'wb') as handle:
+#    dill.dump(rand_mkspans, handle)
+#    
+## Save the speedups so can plot again later...
+#with open('data/rand_speedups.dill', 'wb') as handle:
+#    dill.dump(rand_speedups, handle)
+#    
+## Save the CCRs so can plot again later...
+#with open('data/rand_ccrs.dill', 'wb') as handle:
+#    dill.dump(rand_ccrs, handle)
+#    
+#    
+#"""Plot the speedup over HEFT for the random DAGs."""
+#
+#with open('data/rand_speedups.dill', 'rb') as file:
+#    rand_speedups = dill.load(file)
+#    
+#with open('data/rand_ccrs.dill', 'rb') as file:
+#    rand_ccrs = dill.load(file)
+#    
+#with open('data/rand_mkspans.dill', 'rb') as file:
+#    rand_mkspans = dill.load(file)
+#
+#all_ccrs = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+#all_speedups = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+#
+#for env in [single, multiple]:
+#    for acc in ["low_acc", "high_acc"]:
+#        for ccr in ["0_10", "10_20", "20_50"]:
+#            for i in range(len(rand_mkspans[env.name][acc][ccr]["MST"])):
+#                m = rand_mkspans[env.name][acc][ccr]["HEFT"][i]
+#                for h in heuristics:
+#                    m1 = rand_mkspans[env.name][acc][ccr][h][i]
+#                    d1, d2 = rand_ccrs[env.name][acc][ccr][i], rand_speedups[env.name][acc][ccr][h][i]
+#                    all_ccrs[env.name][acc][h].append(d1)
+#                    all_speedups[env.name][acc][h].append(d2)
+#                    
+#                    
+#with open("results/random/summary.txt", "w") as dest: 
+#    for env in [single, multiple]:  
+#        print("Platform: {}".format(env.name.replace('_', ' ')), file=dest)                
+#        for acc in ["low_acc", "high_acc"]:
+#            print("\n{} ACCELERATION".format("LOW" if acc == "low_acc" else "HIGH"), file=dest)
+#            for h in heuristics:
+#                print("{} mean speedup: {}".format(h, np.mean(all_speedups[env.name][acc][h])), file=dest)
+#        print("\n\n\n\n\n", file=dest)      
                 
 
 #markers = {"HOFT" : '.', "HEFT-WM" : '.'}
